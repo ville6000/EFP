91c91309d19367ff9bdcb2a74066391f2d9afe76
Ville Viklund <ville@viklund.fi>
2015-11-08 13:44:57 +0200

    Read values from file

--- E38/filtering_values.rb
+++ E38/filtering_values.rb
@@ -2,11 +2,23 @@ class FilteringValues
   def run
     puts "Enter a list of numbers, separated by spaces"
     numbers = gets.chomp
-    puts filter_even_numbers(create_array_from_string(numbers)).join(" ")
+    output(filter_even_numbers(create_array_from_string(numbers)).join(" "))
+  end
+
+  def output(even_numbers)
+    puts "The even numbers are #{even_numbers}"
+  end
+
+  def run_from_file(file_name)
+    output(filter_even_numbers(get_values_from_file(file_name)).join(" "))
   end
 
   def create_array_from_string(numbers_string)
     numbers_string.split(/ /).map { |n| n.to_i }
+  end
+
+  def get_values_from_file(file_name)
+    create_array_from_string(File.read(file_name))
   end
 
   # Constraints
--- E38/filtering_values_spec.rb
+++ E38/filtering_values_spec.rb
@@ -9,4 +9,9 @@ RSpec.describe FilteringValues, "#filter_even_numbers" do
     even_numbers = [100, 123, 126, 234, 654]
     expect(f.filter_even_numbers(even_numbers)).to eq([100, 126, 234, 654])
   end
+
+  it "reads values to array from file" do
+    f = FilteringValues.new
+    expect(f.get_values_from_file('values.txt')).to eq([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16])
+  end
 end
